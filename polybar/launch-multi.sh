6d7a126d5b6ad66df29162a39876a23cfca8bf24
Joachim Nielandt <joachim.nielandt@ugent.be>
2018-11-29 16:41:09 +0100

    Set temperature monitor as a global variable in the launch script.

--- polybar/launch-multi.sh
+++ polybar/launch-multi.sh
@@ -9,16 +9,19 @@ while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 # started tray...
 started_tray=false
 
+# set some global variables to be passed to polybar
+export HWMON_PATH=$(ls /sys/devices/platform/coretemp.0/hwmon/hwmon3/temp*_input | head -n 1) 
+
 # launch polybar on all connected monitors
 for m in $(polybar --list-monitors | cut -d":" -f1); do
 	echo "Launching polybar on monitor $m"
-	MONITOR=$m TRAY_POSITION=none HWMON_PATH=$(ls /sys/devices/platform/coretemp.0/hwmon/hwmon3/temp*_input | head -n 1) polybar --reload secondary &
+	MONITOR=$m TRAY_POSITION=none polybar --reload secondary &
 	if [ "$started_tray" = false ]; then
-		MONITOR=$m TRAY_POSITION=right HWMON_PATH=$(ls /sys/devices/platform/coretemp.0/hwmon/hwmon3/temp*_input | head -n 1) polybar --reload example &
+		MONITOR=$m TRAY_POSITION=right polybar --reload example &
 		started_tray=true
 		echo "launching $m with tray"
 	else
-		MONITOR=$m TRAY_POSITION=none HWMON_PATH=$(ls /sys/devices/platform/coretemp.0/hwmon/hwmon3/temp*_input | head -n 1) polybar --reload example &
+		MONITOR=$m TRAY_POSITION=none polybar --reload example &
 		echo "launching $m without tray"
 	fi
 done
