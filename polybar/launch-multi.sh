2c8ad2f0a0037290b87d72bef09c36210851e7cb
Joachim Nielandt <joachim.nielandt@ugent.be>
2018-11-29 15:23:02 +0100

    Added playerctl to the polybar config.

--- polybar/launch-multi.sh
+++ polybar/launch-multi.sh
@@ -6,10 +6,21 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
+# started tray...
+started_tray=false
+
 # launch polybar on all connected monitors
 for m in $(polybar --list-monitors | cut -d":" -f1); do
 	echo "Launching polybar on monitor $m"
-	MONITOR=$m polybar --reload example &
+	if [ "$started_tray" = false ]; then
+		MONITOR=$m TRAY_POSITION=right polybar --reload example &
+		started_tray=true
+		echo "launching $m with tray"
+	else
+		MONITOR=$m TRAY_POSITION=none polybar --reload example &
+		echo "launching $m without tray"
+	fi
+	MONITOR=$m TRAY_POSITION=none polybar --reload secondary &
 done
 
 echo "Done launching."
